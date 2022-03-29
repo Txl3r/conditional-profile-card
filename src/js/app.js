e9209b79a73a353a9c3cb1bfd3037fd24f0c46d4
Txl3r <Harvey.Tyler@live.jobcorps.org>
2022-03-29 19:28:38 +0000

    made changes

--- src/js/app.js
+++ src/js/app.js
@@ -28,28 +28,28 @@ function render(variables = {}) {
   // if includeCover==false then we reset the cover code without the <img> tag to make the cover transparent.
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
-  let ice = variables.name == null ? "Tyler" : variables.name;
-  let fire = variables.lastname == null ? "Harvey" : variables.lastname;
-  let wind = variables.role == null ? "Student" : variables.role;
-  let earth = variables.twitter == null ? "null" : variables.twitter;
-  let kata = variables.github == null ? "Txl3r" : variables.kata;
-  let shigo = variables.linkedin == null ? "null" : variables.linkedin;
-  let kensei = variables.instagram == null ? "null" : variables.instagram;
-  let shinobi = variables.city == null ? "Pilot Rock" : variables.city;
-  let shugoki = variables.country == null ? "OR" : variables.country;
+  let name = variables.name == null ? "Tyler" : variables.name;
+  let lastname = variables.lastname == null ? "Harvey" : variables.lastname;
+  let role = variables.role == null ? "Student" : variables.role;
+  let twitter = variables.twitter == null ? "null" : variables.twitter;
+  let github = variables.github == null ? "Txl3r" : variables.kata;
+  let linkedin = variables.linkedin == null ? "null" : variables.linkedin;
+  let instagram = variables.instagram == null ? "null" : variables.instagram;
+  let city = variables.city == null ? "Pilot Rock" : variables.city;
+  let country = variables.country == null ? "OR" : variables.country;
 
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>${ice} ${fire}</h1>
-          <h2>${wind}</h2>
-          <h3>${shinobi}, ${shugoki}</h3>
+          <h1>${name} ${lastname}</h1>
+          <h2>${role}</h2>
+          <h3>${city}, ${country}</h3>
           <ul class="${variables.socialMediaPosition}">
-            <li><a href="https://twitter.com/${earth}"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/${kata}"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/${shigo}"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/${kensei}"><i class="fab fa-instagram"></i></a></li>
+            <li><a href="https://twitter.com/${twitter}"><i class="fab fa-twitter"></i></a></li>
+            <li><a href="https://github.com/${github}"><i class="fab fa-github"></i></a></li>
+            <li><a href="https://linkedin.com/${linkedin}"><i class="fab fa-linkedin"></i></a></li>
+            <li><a href="https://instagram.com/${instagram}"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
